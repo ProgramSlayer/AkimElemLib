bbc3c12a7027cd0de9adc3ad89552e274158b099
Andrey Pisarets <pisarets.andrey@gmail.com>
2023-04-26 19:25:59 +0300

    Move viewmodel to another file.

--- AkimElemLib.Wpf/ViewModels/CctvCams/StationaryCctvCamListingViewModel.cs
+++ AkimElemLib.Wpf/ViewModels/CctvCams/StationaryCctvCamListingViewModel.cs
@@ -2,7 +2,6 @@
 using AkimElemLib.Wpf.Commands.CctvCams;
 using AkimElemLib.Wpf.DataAccess.Context;
 using AkimElemLib.Wpf.Models.CctvCams;
-using AkimElemLib.Wpf.Models.Common;
 using AkimElemLib.Wpf.Services.DialogWindowServices.Abstractions;
 using AkimElemLib.Wpf.Services.ExceptionHandlers;
 using System;
@@ -71,40 +70,4 @@ public class StationaryCctvCamListingViewModel : ViewModelBase
     {
         _cams.Remove(SelectedStationaryCctvCam!);
     }
-}
-
-public class StationaryCctvCamReadOnlyViewModel : ViewModelBase
-{
-    private readonly StationaryCctvCam _cam;
-
-    public StationaryCctvCamReadOnlyViewModel(StationaryCctvCam cam)
-    {
-        ArgumentNullException.ThrowIfNull(cam, nameof(cam));
-        _cam = cam;
-    }
-
-    public Guid Id => _cam.Id;
-    public double LightSensitivity => _cam.LightSensitivity;
-    public PowerSupplyTypes PowerSupplyType => _cam.PowerSupplyType;
-    public double PowerConsumptionWatts => _cam.PowerConsumptionWatts;
-    public double WorkingVoltage => _cam.WorkingVoltage;
-    public double MatrixDiagonalHorizontal => _cam.MatrixDiagonalHorizontal;
-    public double MatrixDiagonalVertical => _cam.MatrixDiagonalVertical;
-    public double ImageResolutionHorizontal => _cam.ImageResolutionHorizontal;
-    public double ImageResolutionVertical => _cam.ImageResolutionVertical;
-    public string VariofocalLensParams => _cam.VariofocalLensParams.ToString();
-    public double MinTemperature => _cam.MinTemperature;
-    public double MaxTemperature => _cam.MaxTemperature;
-    public CctvCamMatrixFormats MatrixFormat => _cam.MatrixFormat;
-    public double Omega => _cam.Omega;
-    public double Phi => _cam.Phi;
-    public double Alpha => _cam.Alpha;
-    public double AlphaMin => _cam.AlphaMin;
-    public double AlphaMax => _cam.AlphaMax;
-    public double Beta => _cam.Beta;
-    public double InstallationHeight => _cam.InstallationHeight;
-    public bool HasVideoAnalytics => _cam.HasVideoAnalytics;
-    public double Fps => _cam.Fps;
-    public Probability DetectionProbability => _cam.DetectionProbability;
-    public double MinReactionTime => _cam.MinReactionTime;
 }
---
+++ AkimElemLib.Wpf/ViewModels/CctvCams/StationaryCctvCamReadOnlyViewModel.cs
@@ -0,0 +1,42 @@
+﻿using AkimElemLib.Wpf.Models.CctvCams;
+using AkimElemLib.Wpf.Models.Common;
+using System;
+
+namespace AkimElemLib.Wpf.ViewModels.CctvCams;
+
+public class StationaryCctvCamReadOnlyViewModel : ViewModelBase
+{
+    private readonly StationaryCctvCam _cam;
+
+    public StationaryCctvCamReadOnlyViewModel(StationaryCctvCam cam)
+    {
+        ArgumentNullException.ThrowIfNull(cam, nameof(cam));
+        _cam = cam;
+    }
+
+    public Guid Id => _cam.Id;
+    public string Model => _cam.Model;
+    public double LightSensitivity => _cam.LightSensitivity;
+    public PowerSupplyTypes PowerSupplyType => _cam.PowerSupplyType;
+    public double PowerConsumptionWatts => _cam.PowerConsumptionWatts;
+    public double WorkingVoltage => _cam.WorkingVoltage;
+    public double MatrixDiagonalHorizontal => _cam.MatrixDiagonalHorizontal;
+    public double MatrixDiagonalVertical => _cam.MatrixDiagonalVertical;
+    public double ImageResolutionHorizontal => _cam.ImageResolutionHorizontal;
+    public double ImageResolutionVertical => _cam.ImageResolutionVertical;
+    public string VariofocalLensParams => _cam.VariofocalLensParams.ToString();
+    public double MinTemperature => _cam.MinTemperature;
+    public double MaxTemperature => _cam.MaxTemperature;
+    public CctvCamMatrixFormats MatrixFormat => _cam.MatrixFormat;
+    public double Omega => _cam.Omega;
+    public double Phi => _cam.Phi;
+    public double Alpha => _cam.Alpha;
+    public double AlphaMin => _cam.AlphaMin;
+    public double AlphaMax => _cam.AlphaMax;
+    public double Beta => _cam.Beta;
+    public double InstallationHeight => _cam.InstallationHeight;
+    public bool HasVideoAnalytics => _cam.HasVideoAnalytics;
+    public double Fps => _cam.Fps;
+    public Probability DetectionProbability => _cam.DetectionProbability;
+    public double MinReactionTime => _cam.MinReactionTime;
+}
